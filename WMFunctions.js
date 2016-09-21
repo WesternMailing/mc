8a960ed583d488d61093da08db2590b9ae05323a
lines0812 <lines0812@gmail.com>
2016-09-21 15:07:12 +1200

    add initial WMFunctions.js file

---
+++ WMFunctions.js
@@ -0,0 +1,30 @@
+function haveDetailsChanged(form, flagField) {
+
+   // Address check functionality:
+   // 1. grab all values on the details page into one variable
+
+	 	var originalVals = '';
+		var newVals = '';
+		var $inputsToCheck = $(form).find('*[data-update-check="true"]');
+
+		$inputsToCheck.each(function(i) {
+			originalVals += this.val();
+		});
+
+		$inputsToCheck.change(function(){
+			newVals = '';
+
+			$inputsToCheck.each(function(i) {
+				newVals += this.val();
+			});
+
+			if (originalVals != newVals) {
+				$(flagField).val("1");
+			}
+			else
+			{
+				$(flagField).val("0");
+			}
+		});
+
+});
