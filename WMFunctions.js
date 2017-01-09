aa205b592a8fc047e36ad024b4cfb68985fa4c8b
ofransolet <olivier.fransolet@wm.co.nz>
2017-01-10 10:07:25 +1300

    ForceHTTPS
    Add the ForceHTTPS function, this function is required with to enforce
    HTTPS traffic when using 2G campaign

--- WMFunctions.js
+++ WMFunctions.js
@@ -1,30 +1,36 @@
 function haveDetailsChanged(form, flagField) {
 
-   // Address check functionality:
-   // 1. grab all values on the details page into one variable
+    // Address check functionality:
+    // 1. grab all values on the details page into one variable
 
-	 	var originalVals = '';
-		var newVals = '';
-		var $inputsToCheck = $(form).find('*[data-update-check="true"]');
+    var originalVals = '';
+    var newVals = '';
+    var $inputsToCheck = $(form).find('*[data-update-check="true"]');
 
-		$inputsToCheck.each(function(i) {
-			originalVals += this.val();
-		});
+    $inputsToCheck.each(function(i) {
+        originalVals += this.val();
+    });
 
-		$inputsToCheck.change(function(){
-			newVals = '';
+    $inputsToCheck.change(function(){
+        newVals = '';
 
-			$inputsToCheck.each(function(i) {
-				newVals += this.val();
-			});
+        $inputsToCheck.each(function(i) {
+            newVals += this.val();
+        });
 
-			if (originalVals != newVals) {
-				$(flagField).val("1");
-			}
-			else
-			{
-				$(flagField).val("0");
-			}
-		});
+        if (originalVals != newVals) {
+            $(flagField).val("1");
+        }
+        else
+        {
+            $(flagField).val("0");
+        }
+    });
 
-});
+}
+
+function enforceHTTPS() {
+    if (location.protocol != 'https:') {
+        location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    }
+}
