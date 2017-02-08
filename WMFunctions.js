7a68ced86c19233879d143ff0fc32351841cc4cd
Michael Lines <lines0812@gmail.com>
2017-02-08 15:06:54 +1300

    refactor haveDetailsChanged function
    changed haveDetailsChanged function to an object called details with two
    functions, one to collect original data and one to check new data
    against original data.

--- WMFunctions.js
+++ WMFunctions.js
@@ -1,35 +1,35 @@
 // Please use the following url to reference the library in production
 // https://cdn.rawgit.com/WesternMailing/mc/master/WMFunctions.js
 
-function haveDetailsChanged(form, flagField) {
+var details = {
 
-    // Address check functionality:
-    // 1. grab all values on the details page into one variable
-
-    var originalVals = '';
-    var newVals = '';
-    var $inputsToCheck = $(form).find('*[data-update-check="true"]');
-
-    $inputsToCheck.each(function(i) {
-        originalVals += this.val();
-    });
-
-    $inputsToCheck.change(function(){
-        newVals = '';
+    setOriginals: function(form) {
+        var $inputsToCheck = $(form).find('*[data-update-check="true"]');
+        var originalVals = '';
 
         $inputsToCheck.each(function(i) {
-            newVals += this.val();
+            originalVals += this.val();
         });
+    },
 
-        if (originalVals != newVals) {
-            $(flagField).val("1");
-        }
-        else
-        {
-            $(flagField).val("0");
-        }
-    });
+    checkNew: function(form) {
+        var $inputsToCheck = $(form).find('*[data-update-check="true"]');
+        var newVals = '';
 
+        $inputsToCheck.change(function() {
+            newVals = '';
+
+            $inputsToCheck.each(function(i) {
+                newVals += this.val();
+            });
+
+            if (originalVals != newVals) {
+                $(flagField).val("1");
+            } else {
+                $(flagField).val("0");
+            }
+        });
+    }
 }
 
 function enforceHTTPS() {
